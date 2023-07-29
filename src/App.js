e940ac4037ebc502b020031b57985bea350e4a60
ndrbr <andrey.burdak@gmail.com>
2023-07-29 09:54:55 +0700

    Removed post deploy remote check

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello, World!!</p>
+        <p>Hello, World!</p>
         <p>Deployed from GitHub Actions with CodeDeploy</p>
         <a
           className="App-link"
